a3d34f42a1d87b4bef43ed50b257cdb803594654
Camilo Carromeu <camilo@carromeu.com>
2019-10-08 16:54:56 -0400

    Alterando do Axios para o Fetch API para mitigar o CORS.

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,55 @@
 <template>
   <v-app>
     <v-container>
-      <v-layout
-        text-center
-        wrap
-      >
+      <v-layout text-center wrap>
         <v-flex xs12>
-          <v-img
-            :src="require('./assets/logo.svg')"
-            class="my-3"
-            contain
-            height="200"
-          ></v-img>
+          <v-img :src="require('./assets/logo.svg')" class="my-3" contain height="200"></v-img>
         </v-flex>
 
-        <v-flex class="d-flex justify-space-between mb-5" xs12 md10 offset-md-1 xl6 offset-xl-3>
+        <v-flex class="d-flex justify-center mb-10" md6 offset-md-3 offset-xl-4 xl4 xs12>
+          <v-btn
+            :disabled="loading()"
+            :loading="loading()"
+            @click="refresh()"
+            class="ma-2 white--text"
+            color="blue-grey darken-1"
+            x-large
+            dark
+          >
+            Atualizar
+            <v-icon dark right>refresh</v-icon>
+          </v-btn>
+        </v-flex>
+
+        <v-flex class="d-flex justify-space-between mb-5" md10 offset-md-1 offset-xl-3 xl6 xs12>
           <h1 class="headline">Serviços Corporativos</h1>
         </v-flex>
 
-        <v-flex v-if="cCorporate.sum > 0" class="d-flex justify-space-between" xs12 md10 offset-md-1 xl6 offset-xl-3>
+        <v-flex class="d-flex justify-space-between" md10 offset-md-1 offset-xl-3 v-if="cCorporate.sum > 0" xl6 xs12>
           <v-progress-linear indeterminate></v-progress-linear>
         </v-flex>
 
-        <v-flex class="d-flex justify-space-between mb-10" xs12 md10 offset-md-1 xl6 offset-xl-3>
+        <v-flex class="d-flex justify-space-between mb-10" md10 offset-md-1 offset-xl-3 xl6 xs12>
           <v-expansion-panels>
-            <v-expansion-panel v-for="(item,i) in sCorporate" :key="i">
-              <v-expansion-panel-header  disable-icon-rotate>
+            <v-expansion-panel :key="i" v-for="(item,i) in sCorporate">
+              <v-expansion-panel-header disable-icon-rotate>
                 {{ item.name }}
                 <template v-slot:actions>
-                  <v-chip v-show="item.status === undefined" class="py-0" color="yellow darken-3" label text-color="white">
+                  <v-chip
+                    class="py-0"
+                    color="yellow darken-3"
+                    label
+                    text-color="white"
+                    v-show="item.status === undefined || item.status === null"
+                  >
                     Verificando...
                     <v-icon right style="color: #fff;">hourglass_empty</v-icon>
                   </v-chip>
-                  <v-chip v-show="item.status === true" class="py-0" color="green" label text-color="white">
+                  <v-chip class="py-0" color="green" label text-color="white" v-show="item.status === true">
                     Operacional!
                     <v-icon right style="color: #fff;">done</v-icon>
                   </v-chip>
-                  <v-chip v-show="item.status === false" class="py-0" color="red" label text-color="white">
+                  <v-chip class="py-0" color="red" label text-color="white" v-show="item.status === false">
                     Inacessível!
                     <v-icon right style="color: #fff;">error</v-icon>
                   </v-chip>
@@ -49,44 +62,50 @@
           </v-expansion-panels>
         </v-flex>
 
-        <v-flex class="d-flex justify-space-between mb-5" xs12 md10 offset-md-1 xl6 offset-xl-3>
+        <v-flex class="d-flex justify-space-between mb-5" md10 offset-md-1 offset-xl-3 xl6 xs12>
           <h1 class="headline">Unidade Descentralizada</h1>
         </v-flex>
 
-        <v-flex class="d-flex justify-center mb-3" xs12 md6 offset-md-3 xl4 offset-xl-4>
-            <v-select
-              v-model="unity"
-              label="Selecione a unidade..."
-              solo
-              :items="unities"
-              item-text="name"
-              :hint="'Em ' + unity.local"
-              persistent-hint
-              append-outer-icon="location_city"
-              return-object
-              v-on:change="changeUnity()"
-            ></v-select>
+        <v-flex class="d-flex justify-center mb-3" md6 offset-md-3 offset-xl-4 xl4 xs12>
+          <v-select
+            :hint="'Em ' + unity.local"
+            :items="unities"
+            append-outer-icon="location_city"
+            item-text="name"
+            label="Selecione a unidade..."
+            persistent-hint
+            return-object
+            solo
+            v-model="unity"
+            v-on:change="changeUnity()"
+          ></v-select>
         </v-flex>
 
-        <v-flex v-if="cLocal.sum > 0" class="d-flex justify-space-between" xs12 md10 offset-md-1 xl6 offset-xl-3>
+        <v-flex class="d-flex justify-space-between" md10 offset-md-1 offset-xl-3 v-if="cLocal.sum > 0" xl6 xs12>
           <v-progress-linear indeterminate></v-progress-linear>
         </v-flex>
 
-        <v-flex class="d-flex justify-space-between mb-10" xs12 md10 offset-md-1 xl6 offset-xl-3 v-if="unity">
+        <v-flex class="d-flex justify-space-between mb-12" md10 offset-md-1 offset-xl-3 v-if="unity" xl6 xs12>
           <v-expansion-panels>
-            <v-expansion-panel v-for="(item,i) in sLocal[unity.domain]" :key="i">
-              <v-expansion-panel-header  disable-icon-rotate>
+            <v-expansion-panel :key="i" v-for="(item,i) in sLocal[unity.domain]">
+              <v-expansion-panel-header disable-icon-rotate>
                 {{ item.name }}
                 <template v-slot:actions>
-                  <v-chip v-show="item.status === undefined" class="py-0" color="yellow darken-3" label text-color="white">
+                  <v-chip
+                    class="py-0"
+                    color="yellow darken-3"
+                    label
+                    text-color="white"
+                    v-show="item.status === undefined || item.status === null"
+                  >
                     Verificando...
                     <v-icon right style="color: #fff;">hourglass_empty</v-icon>
                   </v-chip>
-                  <v-chip v-show="item.status === true" class="py-0" color="green" label text-color="white">
+                  <v-chip class="py-0" color="green" label text-color="white" v-show="item.status === true">
                     Operacional!
                     <v-icon right style="color: #fff;">done</v-icon>
                   </v-chip>
-                  <v-chip v-show="item.status === false" class="py-0" color="red" label text-color="white">
+                  <v-chip class="py-0" color="red" label text-color="white" v-show="item.status === false">
                     Inacessível!
                     <v-icon right style="color: #fff;">error</v-icon>
                   </v-chip>
@@ -100,25 +119,38 @@
         </v-flex>
       </v-layout>
 
-      <v-dialog v-model="dialog" persistent max-width="290">
+      <v-dialog max-width="290" persistent v-model="dialog">
         <v-card>
           <v-card-text>
-            <p class="text-center pt-4"><v-icon large color="red">cloud_off</v-icon></p>
-            Você está sem conexão com a internet! Ela é necessária para utilizar este serviço. Por favor, verifique sua rede de dados.
+            <p class="text-center pt-4">
+              <v-icon color="red" large>cloud_off</v-icon>
+            </p>Você está sem conexão com a internet! Ela é necessária para utilizar este serviço. Por favor, verifique sua rede de dados.
           </v-card-text>
           <v-card-actions>
             <div class="flex-grow-1"></div>
-            <v-btn color="green darken-1" text @click="reload()">Tentar Novamente</v-btn>
+            <v-btn @click="refresh()" color="green darken-1" text>Tentar Novamente</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
+
+      <v-footer absolute class="font-weight-light" color="blue-grey darken-1" dark padless>
+        <v-col class="text-center" cols="12">
+          {{ new Date().getFullYear() }} &bull;
+          <a
+            href="https://www.embrapa.br/gado-de-corte"
+            style="color: #FFF; text-decoration: none;"
+            target="_blank"
+          >
+            <strong>Embrapa Gado de Corte</strong>
+          </a>
+        </v-col>
+      </v-footer>
     </v-container>
   </v-app>
 </template>
 
 <script>
-import axios from 'axios'
-import https from 'https'
+import _ from 'lodash'
 
 import unities from './settings/unities.json'
 
@@ -128,64 +160,69 @@ import local from './settings/services/local.json'
 
 export default {
   data: () => ({
-    online: false,
     dialog: false,
     unity: null,
-    unities: unities,
+    unities: [],
     sExternal: external,
     sCorporate: corporate,
     sLocal: local,
     cCorporate: { sum: 0 },
     cLocal: { sum: 0 }
   }),
-  beforeMount () {
-    this.unity = unities[0]
-
-    if (typeof window.navigator.onLine === 'undefined') {
-      this.online = true
-    } else {
-      this.online = window.navigator.onLine
+  localStorage: {
+    unity: {
+      type: Object,
+      default: {
+        domain: 'cnpgc',
+        name: 'Embrapa Gado de Corte',
+        local: 'Campo Grande - MS'
+      }
     }
+  },
+  beforeMount () {
+    this.unity = this.$localStorage.get('unity')
+
+    this.unities = _.orderBy(unities, ['name'], ['asc'])
   },
   mounted () {
-    if (this.online) {
-      this.sCorporate.forEach(item => {
-        this.cCorporate.sum++
-
-        console.log('Checking ' + item.url)
-
-        this.check(item, this.cCorporate)
-      })
-
-      this.checkUnitiesServices()
-    } else {
-      this.dialog = true
-    }
+    this.refresh()
   },
   methods: {
-    check (item, counter) {
-      const agent = new https.Agent({
-        rejectUnauthorized: false
-      })
-
-      let options = {
-        httpsAgent: agent,
-        crossdomain: true,
-        mode: 'no-cors',
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        }
+    online () {
+      if (typeof window.navigator.onLine === 'undefined') {
+        return true
+      } else {
+        return window.navigator.onLine
       }
+    },
+    refresh () {
+      if (this.online()) {
+        this.dialog = false
 
-      axios.head(item.url, options).then(response => {
+        this.sCorporate.forEach(item => {
+          this.cCorporate.sum++
+
+          item.status = null
+
+          console.log('Checking ' + item.url)
+
+          this.check(item, this.cCorporate)
+        })
+
+        this.checkUnitiesServices()
+      } else {
+        this.dialog = true
+      }
+    },
+    check (item, counter) {
+      fetch(item.url, { method: 'HEAD', mode: 'no-cors' }).then(response => {
         console.log('Ok to ' + item.url + '!')
         item.status = true
       }).catch(error => {
-        console.log('Failed to ' + item.url + ' - ' + error)
+        console.log('Failed to ' + item.url + ' - ' + JSON.stringify(error))
         item.status = false
       }).finally(() => {
         counter.sum--
-        console.log('counter: ' + counter.sum)
         this.$forceUpdate()
       })
     },
@@ -193,6 +230,8 @@ export default {
       location.reload()
     },
     changeUnity () {
+      this.$localStorage.set('unity', JSON.parse(JSON.stringify(this.unity)))
+
       this.$forceUpdate()
 
       this.checkUnitiesServices()
@@ -202,11 +241,16 @@ export default {
         this.sLocal[this.unity.domain].forEach(item => {
           this.cLocal.sum++
 
+          item.status = null
+
           console.log('Checking ' + item.url)
 
           this.check(item, this.cLocal)
         })
       }
+    },
+    loading () {
+      return this.cCorporate > 0 || this.cLocal > 0
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,28 +4,16 @@ import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
-
-(function () {
-  console.log('CORS Anywhere enabled!')
-
-  var host = 'cors-anywhere.herokuapp.com'
-  var url = 'https://' + host + '/'
-  var slice = [].slice
-  var origin = window.location.protocol + '//' + window.location.host
-  var open = XMLHttpRequest.prototype.open
-  XMLHttpRequest.prototype.open = function () {
-    var args = slice.call(arguments)
-    // eslint-disable-next-line no-useless-escape
-    var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1])
-    if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
-      targetOrigin[1] !== host) {
-      args[1] = url + args[1]
-    }
-    return open.apply(this, args)
-  }
-})()
+import storage from 'vue-localstorage'
 
 Vue.config.productionTip = false
+
+Vue.use(storage)
+
+Vue.use(storage, {
+  name: 'ls',
+  bind: true
+})
 
 new Vue({
   vuetify,
